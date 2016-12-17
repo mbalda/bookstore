367bfa17a4125dd7f54f0c67ac4ccffd9f7fe370
Marcin <marcin.balda@gmail.com>
2016-12-17 20:00:05 +0100

    add links controller.

---
+++ src/Bookstore.Web.API/Controllers/LinksController.cs
@@ -0,0 +1,36 @@
+﻿using Bookstore.Common.Models.WebModels;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bookstore.Web.API.Controllers
+{
+	[RoutePrefix("api")]
+	public class LinksController : ApiController
+	{
+		[Route("")]
+		public IHttpActionResult Get()
+		{
+			IList<Link> links = new List<Link>();
+			var basePath = Request.RequestUri;
+
+			links.Add(
+				new Link
+				{
+					Href = $"{basePath}/books",
+					Rel = "books",
+					Method = HttpMethod.Get.ToString()
+				});
+
+			links.Add(
+				new Link
+				{
+					Href = $"{basePath}/users",
+					Rel = "users",
+					Method = HttpMethod.Get.ToString()
+				});
+
+			return Ok(links);
+		}
+	}
+}
